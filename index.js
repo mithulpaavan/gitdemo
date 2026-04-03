9b47b1fb8168a0d17f0b4206a2bb4a17390783dd
PATRICK <mithulpaavan01@gmail.com>
2026-04-03 11:17:16 +0530

    added saved

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ app.get('/', (req,res)=>{
 
 app.get('/profile', (req, res)=>{
     res.send("profile amigoo");
-    console.log("profile uhh!!")
+    console.log("profile uhh!!");
 })
+
+app.get('/saved', (req, res)=>{
+    res.send("saved")
+    console.log("saved data")
+});
 
 app.listen(5000, ()=>{
     console.log("hello hii...");
