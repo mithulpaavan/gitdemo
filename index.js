728b277c167e2d191cd6e8fef5d19b133b87e0b3
PATRICK <mithulpaavan01@gmail.com>
2026-04-03 12:10:48 +0530

    removed

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.get('/profile', (req, res)=>{
     res.send("profile amigoo");
     console.log("profile uhh!!");
 })
-
 app.get('/saved', (req, res)=>{
     res.send("saved")
     console.log("saved data")
