5e07be55fdea03229cf6b60b7cc7a17397de2523
PATRICK <mithulpaavan01@gmail.com>
2026-04-03 11:09:11 +0530

    hello

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req,res)=>{
     res.send("hola amigoo!!");
 })
 
+app.get('/profile', (req, res)=>{
+    res.send("profile amigoo");
+    console.log("profile uhh!!")
+})
+
 app.listen(5000, ()=>{
     console.log("hello hii...");
 })
